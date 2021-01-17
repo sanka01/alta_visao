456314df4f20ff8f49cb3f05ddbc96f634b07f19
sanka01 <sanka1375@gmail.com>
2021-01-17 12:46:08 -0300

    atualizações layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_005256) do
+ActiveRecord::Schema.define(version: 2021_01_13_185909) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -112,6 +112,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_005256) do
     t.integer "indicacao"
     t.integer "franquia_id"
     t.boolean "status"
+    t.boolean "ativo"
   end
 
   create_table "veiculos", force: :cascade do |t|
